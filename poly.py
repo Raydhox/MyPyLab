aa5f3ae4807f5b16ff73cd5db112aee53f7fb890
Raydhox <38498323+Raydhox@users.noreply.github.com>
2019-01-29 20:21:57 +0100

    Update poly.py

--- poly.py
+++ poly.py
@@ -25,20 +25,23 @@ Create a Polynome class. Exemple: [42,0,1] -> 42 + X**2"""
         if n >= 1:
             P = P + " + " + str(self.coef[1]) + "X"
         if n >= 2:
-            for k in range(n-1, 2, -1):
+            for k in range(2, n):
                 P = P + " + " + str(self.coef[k]) + "X**" + str(k)
         return P
 
     def __getitem__(self, index):
         """Return index """
-        return self.coef[index]
+        try:
+            return self.coef[index]
+        except IndexError:
+            return 0
 
     def __add__(self, Q):
         """Add P+Q, P is the current Polynome."""
         S = []
-        n = min([self.deg, Q.deg])
+        n = max([self.deg, Q.deg])
         for k in range(n):
-            S.append(self.coef[k] + Q.coef[k])
-        return Polynome(S + self.coef[n:] + Q[n:])
+            S.append(self[k] + Q.coef[k])
+        return Poly(S)
     
-P = Polynome( [42, 1, 1] )
+P = Poly( [42, 1, 1] )
