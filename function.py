996d4a358c9f9b92d3174303fe0b90bc4b63024c
Raydhox <38498323+Raydhox@users.noreply.github.com>
2019-10-11 23:34:03 +0200

    Update function.py

--- function.py
+++ function.py
@@ -3,41 +3,42 @@
 
 class Fun:
 
-    def __init__(self, f, fx="f"):
+    def __init__(self, function, name="f"):
         """Create a function Fun where:
 f = lambda x : f(x)
 fx is a string which represents f(x)."""
-        self.f = f
-        self.fx = fx
-
+        self.function = function
+        self.name = name
+        
     def __repr__(self):
-        return self.fx
-
-##    def __eq__(self, g):
-##        """???"""
-##        pass
+        return "Fun: x |--> %s(x)" %(self.name)
 
     #===Operations===
     def __add__(self, g):
         """Return the function f+g, f is the current function."""
-        return Fun( lambda x : self(x) + g(x) )
+        name = "(%s + %s)" %(self.name, g.name)
+        return Fun( lambda x : self(x) + g(x), name )
 
     def __sub__(self, g):
         """Return the function f-g, f is the current function."""
-        return Fun( lambda x : self(x) - g(x) )
+        name = "(%s - %s)" %(self.name, g.name)
+        return Fun( lambda x : self(x) - g(x), name )
 
     def __rmul__(self, k):
         """K is a real or a complex. Return the function k*f, f is the current function."""
         if type(k) in [int, float, complex]:
-            return Fun( lambda x : k*self(x) )
+            name = "{}{}".format(k, self.name)
+            return Fun( lambda x : k*self(x), name )
 
-    def __mul__(self, Q):
+    def __mul__(self, g):
         """Return the function f*g, f is the current function."""
-        return Fun( lambda x : self(x) * g(x) )
+        name = "(%s * %s)" %(self.name, g.name)
+        return Fun( lambda x : self(x) * g(x), name )
 
-    def __truediv__(self, Q):
+    def __truediv__(self, g):
         """Return the function f/g, f is the current function."""
-        return Fun( lambda x : self(x) * g(x) )
+        name = "(%s / %s)" %(self.name, g.name)
+        return Fun( lambda x : self(x) / g(x), name )
 
     def __pow__(self, n):
         """Return the function fofofo...of, f is the current function."""
@@ -47,11 +48,30 @@ fx is a string which represents f(x)."""
             for k in range(n):
                 y = f(y)
             return y
-        return Fun( lambda x: fn(self.f, n, x)) )
+        name = "%s^(%d)" %(self.name, n)
+        return Fun( lambda x: fn(self.function, n, x), name )
 
-    def __call__(self, Q):
-        """Return the function fof,  f is the current function."""
-        return Fun( lambda x : self(self)(x) )
+    def __call__(self, g):
+        """Return the function fog if g is a function Fun,
+else return f(g), f is the current function."""
+        if type(g) == Fun:
+            name = "(%s o %s)" %(self.name, g.name)
+            return Fun( lambda x : self(g(x)), name )
+        else:
+            return self.function(g)
+
+Id = Fun(lambda x : x, "Id")
 
 if __name__ == "__main__" :
-    f = Fun(lambda x : x**2)
+    f = Fun(lambda x : x**2, ".**2")
+
+
+
+
+
+
+
+
+
+
+    
